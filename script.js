380b52148de84c78c2e543784e1f102d8f311b06
jairisonsouza <jairo.tinformatica@gmail.com>
2023-09-03 16:55:46 -0300

    Alterando títulos ao clicar nos botões de descanso

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBtn = document.querySelector('.app__card-button--foco')
 const curtoBtn = document.querySelector('.app__card-button--curto')
 const longoBtn = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 focoBtn.addEventListener('click', () => {
     alterarContexto('foco')
@@ -19,5 +20,27 @@ longoBtn.addEventListener('click', () => {
 function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada,<br>
+                <strong class="app__title-strong">faça uma pausa curta.</strong>
+            `
+            break
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície,<br>
+                <strong class="app__title-strong">faça uma pausa longa.</strong>
+            `
+            break
+        default:
+            break
+    }
 }
 
